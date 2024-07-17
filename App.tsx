e6d5f9f4389ea0824602f7227340035ad196b008
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2024-07-16 21:53:14 -0300

    text commit

--- App.tsx
+++ App.tsx
@@ -4,11 +4,13 @@ import { GluestackUIProvider, SafeAreaView } from "@gluestack-ui/themed";
 import Home from "./src/screens/Home";
 import Alert from "./src/components/Alert";
 import CreditCard from "./src/components/CreditCard";
+import TextTitle from "./src/components/TextTitle";
 
 export default function App() {
   return (
     <GluestackUIProvider config={config}>
       <SafeAreaView flex={1}>
+        <TextTitle />
         <Home />
         <Alert />
         <CreditCard />
---
+++ src/components/TextTitle.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Center, Text } from "@gluestack-ui/themed";
+
+const TextTitle = () => {
+  return (
+    <Center h={100}  >
+      <Text size={"2xl"} fontWeight={"bold"}>
+        Hola Facu
+      </Text>
+    </Center>
+  );
+};
+export default TextTitle;
